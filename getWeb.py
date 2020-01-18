e4a2336183b62424d7b72c079bd50219a334c6da
Ian <ha0511kr.com@gmail.com>
2020-01-18 15:47:11 -0700

    Added code inorder to connect the webcam to the computer

--- getWeb.py
+++ getWeb.py
@@ -4,8 +4,16 @@ camera = cv2.VideoCapture(0);
 
 cv2.namedWindow("Image");
 
-ret, frame = camera.read();
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 28);
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 28);
 
-cv2.imshow("Image", frame);
+while True:
+	ret, frame = camera.read();
+	gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+	cv2.imshow("Image", gray_image);
+	cv2.imwrite("Image.jpg", frame);
+	if cv2.waitKey(30) & 0xFF == ord('q'):
+        	break
 
-
+camera.release();
+cv2.destroyAllWindows();
