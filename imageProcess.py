d3aa043a946b2195d4321f261f4c9003e9c2f8e6
Ian <ha0511kr.com@gmail.com>
2020-01-18 17:01:43 -0700

    Added an image processing code

---
+++ imageProcess.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+
+while(True):
+    folder = os.listdir("dataset-resized");
+    for a in folder:
+        if(a != ".DS_Store"):
+            mylist = os.listdir("dataset-resized/" + a);
+            for b in mylist:
+                img = cv2.imread("dataset-resized/" + a + "/" + b);
+                grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
+                grayImg = cv2.resize(grayImg, (150, 150));
+                cv2.imshow("image", grayImg);
+                cv2.waitKey(0);
+        
