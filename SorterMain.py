67c0adccb76f7e85c3eb3b563d6eaba2d73e12ba
Nicholas Chin <nic.c3.14@gmail.com>
2020-01-19 09:01:33 -0700

    Created main project script

---
+++ SorterMain.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import cv2
+import os
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
+import random
+import serial
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+def send_class(classification):
+    if (classification == "garbage"):
+        ser.write(b'g')
+        print("garbage")
+    elif (classification == "recycle"):
+        ser.write(b'r')
+        print("recycle")
+    elif (classification == "compost"):
+        ser.write(b'c')
+        print("compost")
+
+
+camera = cv2.VideoCapture(0);
+
+cv2.namedWindow("Image");
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 150);
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 150);
+
+class_names = ['cardboard','glass','metal','paper','plastic','trash']
+
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation = "relu", input_shape = (150,150,3)),
+    keras.layers.Conv2D(32, (3, 3), activation = "relu"),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(32, (3, 3), activation = "relu"),
+    keras.layers.Conv2D(32, (3, 3), activation = "relu"),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Flatten(),
+    keras.layers.Dense(256, activation = "relu"),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(6, activation = "softmax")
+])
+model.compile(optimizer = "adam", loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+model.load_weights('./modelWeights')
+
+bins = ['recycle','recycle','recycle','recycle','recycle','trash']
+while True:
+    ret, frame = camera.read();
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB);
+    frame = cv2.resize(frame, (150, 150));
+    camera_images = []
+    camera_images.append(frame)
+
+    camera_images = np.array(camera_images,np.uint8);
+    camera_images = camera_images/255.0
+
+    prediction = model.predict(camera_images)
+    send_class(prediction[0])
+    input("press enter to continue")
+camera.release();
+cv2.destroyAllWindows();
